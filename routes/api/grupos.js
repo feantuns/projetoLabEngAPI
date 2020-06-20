14f7e83d71c87272d218ebf26d6a689244049452
Felipe Antunes <felipe.antunes@assertivainformacoes.com>
2020-06-19 22:11:14 -0300

    edit delete grupos

--- routes/api/grupos.js
+++ routes/api/grupos.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Grupo = require('../../models/Grupo');
+const MembroGrupo = require('../../models/MembroGrupo');
 const { ensureAuthenticated } = require('../../config/auth');
 
 // Get list of groups
@@ -23,6 +24,79 @@ router.post('/', ensureAuthenticated, (req, res) => {
       console.log(err);
       return res.sendStatus(500);
     });
+});
+
+// Edit a grupo
+router.put('/:grupoId', ensureAuthenticated, async (req, res) => {
+  const { grupoId } = req.params;
+  const { nome, modalidade } = req.body;
+  let errors = [];
+
+  if (!grupoId) {
+    errors.push({ message: 'Informe o id de um grupo.' });
+  }
+
+  const grupoInformado = await Grupo.findOne({ where: { id: grupoId } });
+
+  if (!grupoInformado) {
+    errors.push({ message: 'O grupo informado não existe.' });
+  }
+
+  if (errors.length > 0) {
+    return res.status(400).json(errors);
+  }
+
+  const updates = {
+    nome: nome || grupoInformado.nome,
+    modalidade: modalidade || grupoInformado.modalidade,
+  };
+
+  await Grupo.update({ ...updates }, { where: { id: grupoId } });
+  res.sendStatus(204);
+});
+
+// Delete a grupo
+router.delete('/:grupoId', ensureAuthenticated, async (req, res) => {
+  const { grupoId } = req.params;
+  let errors = [];
+
+  if (!grupoId) {
+    errors.push({ message: 'Informe o id de um grupo.' });
+  }
+
+  const grupoInformado = await Grupo.findOne({ where: { id: grupoId } });
+
+  if (!grupoInformado) {
+    errors.push({ message: 'O grupo informado não existe.' });
+  }
+
+  if (errors.length > 0) {
+    return res.status(400).json(errors);
+  }
+
+  const deleteConnectionsWithMembros = await MembroGrupo.destroy({
+    where: {
+      grupo_id: grupoId,
+    },
+  });
+
+  if (!deleteConnectionsWithMembros) {
+    errors.push({ message: 'Erro ao excluir conexões de grupo com membro.' });
+    return res.status(500).json(errors);
+  }
+
+  const deleteGrupo = await Grupo.destroy({
+    where: {
+      id: grupoId,
+    },
+  });
+
+  if (!deleteGrupo) {
+    errors.push({ message: 'Erro ao excluir grupo.' });
+    return res.status(500).json(errors);
+  }
+
+  return res.sendStatus(204);
 });
 
 module.exports = router;
